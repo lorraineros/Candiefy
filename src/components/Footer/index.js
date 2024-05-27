b82c18aa906e0d5df47106596766ad972aeaf5e5
lorraineros <lorrainert2002@gmail.com>
2024-05-27 00:38:06 +0000

    Changed some styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import './styles.css';
 export const Footer = () => {
   return (
     <footer className="footer">
-      <span>Copyright &copy; 2024. All rights are reserved.</span>
+      <span>Copyright &copy; Candiefy 2024. All rights are reserved.</span>
     </footer>
   )
 };
