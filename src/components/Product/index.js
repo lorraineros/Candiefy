58b1213fe69f0592832202f026c8e58b63443c2a
lorraineros <lorrainert2002@gmail.com>
2024-06-09 20:51:04 +0000

    Update

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import LazyLoad from 'react-lazyload';
 import './styles.css';
 import AddToCartPopup from '../AddToCart';
 
 export const Product = ({ id, name, price, img, addToCart }) => {
   const [showPopup, setShowPopup] = useState(false);
-  
+
   const handleClick = () => {
     addToCart({ id, name, price, img }, 1);
     setShowPopup(true);
@@ -13,7 +14,6 @@ export const Product = ({ id, name, price, img, addToCart }) => {
     }, 3000);
     return () => clearTimeout(timeout);
   };
-  
 
   const handleClosePopup = () => {
     setShowPopup(false);
@@ -23,7 +23,9 @@ export const Product = ({ id, name, price, img, addToCart }) => {
     <div className="product">
       <a href={`/product/${id}`} className="product-link-wrapper">
         <div>
-          <img alt={name} src={img} className="product-image" loading="lazy" />
+          <LazyLoad height={200} offset={100}>
+            <img alt={name} src={img} className="product-image" />
+          </LazyLoad>
           <h3 className="product-name">{name}</h3>
           <p className="product-price">{price} kr.</p>
         </div>
@@ -31,7 +33,7 @@ export const Product = ({ id, name, price, img, addToCart }) => {
       <button className="product-button" onClick={handleClick}>
         Add To Cart
       </button>
-      {showPopup && <AddToCartPopup name={name} price={price} img={img} quantity={1} handleClosePopup={handleClosePopup}/>}
+      {showPopup && <AddToCartPopup name={name} price={price} img={img} quantity={1} handleClosePopup={handleClosePopup} />}
     </div>
   );
 };
